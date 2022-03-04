d02e66d1da627c653800712be8fd808f7c632c7b
toannnchainos <toannn@chainos.vn>
2022-03-04 07:35:02 +0700

    fix update error

--- contracts_hardhat/test/test.ts
+++ contracts_hardhat/test/test.ts
@@ -1,72 +1,115 @@
 import "@nomiclabs/hardhat-waffle";
-import { ethers } from 'hardhat';
+import { ethers  } from 'hardhat';
 import { expect, use, util } from 'chai';
+import { BigNumberish, PayableOverrides, utils, ContractTransaction } from 'ethers'
 import { solidity } from 'ethereum-waffle';
 
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address"
 
-import { BlockhashStore } from "typechain/BlockhashStore";
-import { LinkToken } from "typechain/LinkToken";
-import { VRFCoordinator } from "typechain/VRFCoordinator";
-import { RandomNumberGenerator } from "typechain/RandomNumberGenerator";
-import { CakeToken } from "typechain/CakeToken";
-import { PancakeSwapLottery } from "typechain/PancakeSwapLottery";
+import { Lottery } from "typechain/Lottery";
 
 import { base58 } from "ethers/lib/utils";
 use(solidity);
 
-const price = 1000
+export const usedGasEther = async (res: ContractTransaction): Promise<BigNumberish> => {
+  const usedGas = (await res.wait(0)).cumulativeGasUsed
 
-describe('Marketplace ', () => {
-  let BlockhashStoreInstance: BlockhashStore
-  let LinkTokenInstance: LinkToken
-  let VRFCoordinatorInstance: VRFCoordinator
-  let RandomNumberGeneratorInstance: RandomNumberGenerator
-  let CakeTokenInstance: CakeToken
-  let PancakeSwapLotteryInstance: PancakeSwapLottery
+  return new Promise((resolve, reject) => {
+    resolve(usedGas.mul(res.gasPrice));
+  });
+}
+
+const price = ethers.utils.parseEther("0.2")
+const priceBigger = ethers.utils.parseEther("0.3")
+const priceSmaller = ethers.utils.parseEther("0.1")
+const currentBlock1 = ethers.provider.blockNumber + 100;
+const currentBlock2 = ethers.provider.blockNumber + 200;
+const currentBlock3 = ethers.provider.blockNumber + 300;
+const currentBlock4 = ethers.provider.blockNumber + 400;
+
+async function mineNBlocks(n) {
+  const minute = 3600
+  for (let index = 0; index < n; index++) {
+    await ethers.provider.send('evm_increaseTime', [minute]);
+    await ethers.provider.send('evm_mine', []);
+  }
+}
+
+describe('lottery ', () => {
+  let LotteryInstance: Lottery
 
   let owner: SignerWithAddress
   let addr1: SignerWithAddress
   let addr2: SignerWithAddress
-  let addressFee: SignerWithAddress
-  let exchanger: SignerWithAddress
+  let addr3: SignerWithAddress
+  let addr4: SignerWithAddress
 
   beforeEach(async () => {
-    [owner, addr1, addr2, exchanger, addressFee] = await ethers.getSigners();
-    const BlockhashStoreContract= await ethers.getContractFactory("BlockhashStore")
-    BlockhashStoreInstance = (await BlockhashStoreContract.deploy()) as BlockhashStore
-    await BlockhashStoreInstance.deployed();
-
-    const LinkTokenContract= await ethers.getContractFactory("LinkToken")
-    LinkTokenInstance = (await LinkTokenContract.deploy()) as LinkToken
-    await LinkTokenInstance.deployed();
-
-    const VRFCoordinatorContract= await ethers.getContractFactory("VRFCoordinator")
-    VRFCoordinatorInstance = (await VRFCoordinatorContract.deploy(LinkTokenInstance.address, BlockhashStoreInstance.address)) as VRFCoordinator
-    await VRFCoordinatorInstance.deployed();
-
-    const RandomNumberGeneratorContract= await ethers.getContractFactory("RandomNumberGenerator")
-    RandomNumberGeneratorInstance = (await RandomNumberGeneratorContract.deploy(VRFCoordinatorInstance.address, LinkTokenInstance.address) as RandomNumberGenerator)
-    await RandomNumberGeneratorInstance.deployed();
-
-    const CakeTokenContract= await ethers.getContractFactory("CakeToken")
-    CakeTokenInstance = (await CakeTokenContract.deploy() as CakeToken)
-    await CakeTokenInstance.deployed();
-
-    const PancakeSwapLotteryContract= await ethers.getContractFactory("PancakeSwapLottery")
-    PancakeSwapLotteryInstance = (await PancakeSwapLotteryContract.deploy(CakeTokenInstance.address, RandomNumberGeneratorInstance.address) as PancakeSwapLottery)
-    await PancakeSwapLotteryInstance.deployed();
+    [owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
+    const LotteryContract= await ethers.getContractFactory("Lottery")
+    LotteryInstance = (await LotteryContract.deploy()) as Lottery
+    await LotteryInstance.deployed();
 
   });
-  describe('test farming pool', () => {
-    beforeEach("create data ", async () => {
-        CakeTokenInstance["mint(address,uint256)"](PancakeSwapLotteryInstance.address, ethers.utils.parseEther("10000"));
-        PancakeSwapLotteryInstance.maxPriceTicketInCake();
-        
+  describe('test lottery', () => {
+    beforeEach("init pool ", async () => {
+      await LotteryInstance.generatePoolLottery(price, currentBlock1);
+      await expect(LotteryInstance.generatePoolLottery(price, currentBlock1)).to.be.revertedWith("poolData is not expires");
     });
 
-    it("create stake ", async () => {
+    it("buy tickket ", async () => {
+        const balanceAdd1ETHBefore = await ethers.provider.getBalance(addr1.address);
+        const balanceAdd2ETHBefore = await ethers.provider.getBalance(addr2.address);
 
+        await expect(LotteryInstance.connect(addr1).buyTicket({
+          value: priceBigger
+        })).to.be.revertedWith("price must samge price ticket");
+
+        await expect(LotteryInstance.connect(addr1).buyTicket({
+          value: priceSmaller
+        })).to.be.revertedWith("price must samge price ticket")
+
+       
+        const tx2 = await LotteryInstance.connect(addr2).buyTicket({
+          value: price
+        })
+        const tx3 = await LotteryInstance.connect(addr2).buyTicket({
+          value: price
+        })
+
+        const costTx2 = await usedGasEther(tx2);
+        const costTx3 = await usedGasEther(tx3);
+        const balanceAdd2ETHAfter = await ethers.provider.getBalance(addr2.address);
+
+        expect(balanceAdd2ETHBefore).to.be.equal(balanceAdd2ETHAfter.add(price).add(price).add(costTx2).add(costTx3))
+    });
+
+    it("pick winner ", async () => {
+      await LotteryInstance.connect(addr1).buyTicket({
+        value: price
+      })  
+      await LotteryInstance.connect(addr2).buyTicket({
+        value: price
+      })
+      await LotteryInstance.connect(addr2).buyTicket({
+        value: price
+      })
+      await LotteryInstance.connect(addr3).buyTicket({
+        value: price
+      })  
+      await LotteryInstance.connect(addr4).buyTicket({
+        value: price
+      })
+      await LotteryInstance.connect(addr4).buyTicket({
+        value: price
+      })
+
+      await expect(LotteryInstance.connect(addr4).pickWinner()).to.be.revertedWith("Ownable: caller is not the owner");
+      await expect(LotteryInstance.connect(owner).pickWinner()).to.be.revertedWith("poolData is not expires");
+      await mineNBlocks(300);
+
+      await LotteryInstance.connect(owner).pickWinner();
+      await expect(LotteryInstance.connect(owner).pickWinner()).to.be.revertedWith("poolData is done");
     });
   })
 })
